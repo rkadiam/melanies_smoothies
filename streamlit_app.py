31cee376827f4b93bbcb0c0191f79528b6cd91fd
rkadiam <rkadiam@scanhealthplan.com>
2025-01-31 20:53:40 -0800

    Update streamlit_app.py
    Put json into a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,9 @@ ingredients_list = st.multiselect(
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 ingredients_string = ''
 
 #st.write(ingredients_list)
